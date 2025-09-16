4bff110c188790ed27777e7bc906e2522e8b5bbb
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-09-16 18:06:38 +0200

    feat: update SatelliteManager for toggle able satellite rendering

--- src/SatelliteManager.cpp
+++ src/SatelliteManager.cpp
@@ -38,7 +38,35 @@ void SatelliteManager::updateAllPositions(TLEtoGPSConverter* converter) {
 std::vector<GPSPosition> SatelliteManager::getPositions() const {
     std::vector<GPSPosition> positions;
     for (const auto& sat : satellites) {
-        positions.push_back(sat.getPosition());
+        // only updates visible satellites
+        if (sat.isVisible()) {
+            positions.push_back(sat.getPosition());
+        }
     }
     return positions;
+}
+
+std::vector<std::string> SatelliteManager::getSatelliteNames() const {
+    std::vector<std::string> names;
+    for (const auto& sat: satellites) {
+        names.push_back(sat.getName());
+    }
+    return names;
+}
+
+void SatelliteManager::toggleSatellite(int index) {
+    if (index >= 0 && index < satellites.size()) {
+        satellites[index].setIsVisible(!satellites[index].isVisible());
+    }
+}
+
+bool SatelliteManager::isSatelliteVisible(int index) const {
+    if (index >= 0 && index < satellites.size()) {
+        return satellites[index].isVisible();
+    }
+    return false;
+}
+
+size_t SatelliteManager::getSatelliteCount() const {
+    return satellites.size();
 }
